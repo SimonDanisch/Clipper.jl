ba26c7fdccde8e728f00a3bc699dd25511fb04e7
SimonDanisch <sdanisch@gmail.com>
2016-12-22 15:47:44 +0100

    clean up build and add WinRPM for windows

--- deps/build.jl
+++ deps/build.jl
@@ -1,40 +1,39 @@
-#! /usr/bin/env julia
-
-path = joinpath(dirname(@__FILE__), "..", "src")
-
-if is_windows()
-    if success(`where cl.exe`)
-        info("Compiling with cl.exe")
-        run(`cl.exe /D_USRDLL /D_WINDLL /EHsc /Fo$(path) $(path)cclipper.cpp $(path)clipper.cpp /MT /link /DLL /OUT:$(path)cclipper.dll`)
-    else
-        import WinRPM
-        info("Compiling with WinRPM gcc-c++")
-        WinRPM.install("gcc-c++"; yes = true)
-        WinRPM.install("gcc"; yes = true)
-        WinRPM.install("headers"; yes = true)
-
+function find_gpp()
+    @static if is_windows()
+        #TODO, is this really the hardcoded way to get the WinRPM path to g++ ?
         gpp = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","bin","g++")
+        if !isfile(gpp*".exe")
+            info("g++ not found. installing gcc-c++ using WinRPM")
+            @eval using WinRPM
+            WinRPM.install("gcc-c++"; yes = true)
+            WinRPM.install("gcc"; yes = true)
+            WinRPM.install("headers"; yes = true)
+        end
         RPMbindir = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","bin")
         incdir = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","include")
-
-        push!(Base.Libdl.DL_LOAD_PATH, RPMbindir)
-        ENV["PATH"] = ENV["PATH"] * ";" * RPMbindir
-        if success(`$gpp --version`)
-            cd(path) do
-                run(`$gpp -c -fPIC -std=c++11 clipper.cpp cclipper.cpp -I $incdir`)
-                run(`$gpp -shared -o cclipper.dll clipper.o cclipper.o`)
-            end
+        push!(Base.Libdl.DL_LOAD_PATH, RPMbindir) # TODO does this need to be reversed?
+        ENV["PATH"] = ENV["PATH"] * ";" * RPMbindir;
+        return gpp, incdir, "dll"
+    end
+    @static if is_unix()
+        if success(`g++ --version`)
+            return "g++", "", "so"
         else
-            error("no windows c++ compiler (cl.exe) found, and WinRPM with g++ is failing as well.")
+            error("no g++ found. Please install a version > 4.5")
         end
     end
 end
 
-if is_unix()
+path = joinpath(dirname(@__FILE__), "..", "src")
+
+if success(`where cl.exe`) && iswindows()
+    info("Compiling with cl.exe")
+    run(`cl.exe /D_USRDLL /D_WINDLL /EHsc /Fo$(path) $(path)cclipper.cpp $(path)clipper.cpp /MT /link /DLL /OUT:$(path)cclipper.dll`)
+else
+    # Note: on Mac OS X, g++ is aliased to clang++.
+    gpp, incdir, ext = find_gpp()
     cd(path) do
-        # Note: on Mac OS X, g++ is aliased to clang++.
-        run(`g++ -c -fPIC -std=c++11 clipper.cpp cclipper.cpp`)
-        run(`g++ -shared -o cclipper.so clipper.o cclipper.o`)
+        run(`$gpp -c -fPIC -std=c++11 clipper.cpp cclipper.cpp -I $incdir`)
+        run(`$gpp -shared -o cclipper.$ext clipper.o cclipper.o`)
     end
 end
-
